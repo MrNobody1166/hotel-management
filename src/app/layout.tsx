ca352712dfc786f8d6a84295b750f8e8314ee721
CalebPickens <calebpickens1166@gmail.com>
2023-12-22 14:40:59 -0600

    Add dark mode theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Poppins } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header/Header'
 import Footer from "@/components/Footer/Footer"
+import ThemeProvider from '@/components/ThemeProvider/ThemeProvider';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -25,11 +26,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main className='font-normal'>
-          <Header />
-          {children}
-          <Footer />
+        <ThemeProvider>
+          <main className='font-normal'>
+            <Header />
+            {children}
+            <Footer />
           </main>
+        </ThemeProvider>
       </body>
     </html>
   );
