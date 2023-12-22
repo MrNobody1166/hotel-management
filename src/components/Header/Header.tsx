18c1ef4e14bccebe134fd256e9d0177a221f18de
CalebPickens <calebpickens1166@gmail.com>
2023-12-22 13:31:25 -0600

    Add Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import {MdDarkMode} from "react-icons/md"
 const Header = () => {
   return <header className="py10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
       <div className="flex items-center w-full md:2/3">
-        <Link href='/' className="font-black text-[#F27405]">
+        <Link href='/' className="font-black text-tertiary-dark">
           Hotelzz
         </Link>
         <ul className="flex items-center ml-5">
