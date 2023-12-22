6fd51a75442cfe62cb0d820b1a9debe097a1e4ac
CalebPickens <calebpickens1166@gmail.com>
2023-12-22 15:59:24 -0600

    Create sign up page

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 
-import ThemeContext from "@/context/themeContext";
+import ThemeContext from "@/context/ThemeContext";
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const themeFromStorage: boolean =
